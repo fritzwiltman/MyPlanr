69a95250cd2d33ff0279bd1964719aae452e33e7
Fritz Wiltman <fgwilt1@gmail.com>
2019-04-28 14:06:21 -0400

    s

---
+++ MyPlanr/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MyPlanr
+//
+//  Created by Fritz on 4/28/19.
+//  Copyright © 2019 Fritz Gerald Wiltman. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
