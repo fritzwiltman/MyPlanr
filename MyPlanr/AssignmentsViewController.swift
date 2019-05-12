ebde2d632ccc8ca377097b4eabc6f515c332d700
Fritz Wiltman <fgwilt1@gmail.com>
2019-05-11 21:39:44 -0400

    trying again

--- MyPlanr/AssignmentsViewController.swift
+++ MyPlanr/AssignmentsViewController.swift
@@ -8,16 +8,52 @@
 
 import UIKit
 
-class AssignmentsViewController: UIViewController {
+class AssignmentsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    @IBAction func ButtonAction(_ sender: Any) {
-        print("Welcome to Assignments tab.")
+    var assignments : [(DueDate: String, Class: String, Assignment: String)] = []
+    
+    @IBOutlet weak var tableViewOutlet: UITableView! {
+        didSet {
+            tableViewOutlet.dataSource = self
+            tableViewOutlet.delegate = self
+        }
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        update()
+        
         // Do any additional setup after loading the view.
     }
+    
+    // This will be variable depending on the number of assignments on a given day
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return assignments.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableViewOutlet.dequeueReusableCell(withIdentifier: "AssignmentCell", for: indexPath)
+        
+        if let myCell = cell as? AssignmentCell {
+            myCell.DueDateTime.text = assignments[indexPath.row].0
+            myCell.Class.text = assignments[indexPath.row].1
+            myCell.Assignment.text = assignments[indexPath.row].2
+            
+            
+        }
+        return cell
+    }
+    
+    // There should be as many sections as there are class/days in the week
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func update() {
+        assignments = [("04/31/2019", "CMSC436", "Assignment 5"), ("04/31/2019", "CMSC434", "Midterm"), ("05/02/2019", "FMSC110", "In-class assignment")]
+        return
+    }
+    
     
 
     /*
@@ -30,4 +66,13 @@ class AssignmentsViewController: UIViewController {
     }
     */
 
+}
+
+
+
+class AssignmentCell : UITableViewCell {
+    @IBOutlet weak var Class: UILabel!
+    @IBOutlet weak var Assignment: UILabel!
+    @IBOutlet weak var DueDateTime: UILabel!
+    
 }
--- MyPlanr/assignments.swift
+++ MyPlanr/assignments.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+import UIKit
+
+struct Assignment {
+    var name : String
+    var assignmentType : AssignmentType
+    var course : Course
+    var dueDate : DateComponents // Might have to change this to string MM/DD/YYYY
+    var notes : String
+}
+
+struct Course {
+    var name : String
+    var color : UIColor
+}
+
+enum AssignmentType : CaseIterable {
+    case homework, test, study, read, paper, presentation, project, lab, final, midterm, quiz, other
+}
+
+class Assignments {
+    var assignments : [DateComponents : Assignment]
+    
+    init() {
+        assignments = [:]
+    }
+    
+    func newAssignment(nm : String, assignType : AssignmentType, crs : Course, date : DateComponents, nts : String) {
+        Assignment(name: nm, assignmentType: assignType, course: crs, dueDate: date, notes: nts)
+        
+    }
+}
